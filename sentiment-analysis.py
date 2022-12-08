f75201a0a47da3613a9f43b713ab999338c91ab2
Omar <ramoalam@live.com>
2022-12-08 10:04:24 +0100

    Loaded model and tokenizer

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,6 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from scipy.special import softmax
 
+# Load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+labels = ['Negative', 'Neutral', 'Positive']
+
 # Remove strings that start with @ and are longer than 1 char (likely Twitter handles)
 def drop_handles(word):
     if word.startswith('@') and len(word) > 1:
